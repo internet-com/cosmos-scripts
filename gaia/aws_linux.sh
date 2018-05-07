242ad9520551bea46d0c62bf2ff8c6877d5d76e9
Henrik Aasted Sørensen <has@bitcraft.dk>
2018-05-07 15:27:44 +0200

    Support for testnet-5000

--- gaia/aws_linux.sh
+++ gaia/aws_linux.sh
@@ -1,8 +1,19 @@
 #!/bin/bash -ex
 
+# Current source of documentation: https://cosmos.network/validators/tutorial
+# Requires Amazon Linux 2
+
+# The git tag from https://github.com/cosmos/cosmos-sdk
+RELEASETAG="v0.16.0-rc0"
+
+# The testnet to join (https://github.com/cosmos/testnets/)
+TESTNET="gaia-5000"
+
+MONIKER="XXX"
+
 # Debug logging
-# exec > /var/log/gaia-setup.log
-# exec 2>&1
+exec > /var/log/gaia-setup.log
+exec 2>&1
 
 # Update packages - we'll reboot later on in case a this includes a kernel update
 yum update -y
@@ -33,18 +44,34 @@ export GOPATH=/opt/gopath
 export PATH=$GOPATH/bin:/usr/local/go/bin:$PATH
 
 # Install Gaia 0.5.0
-go get github.com/cosmos/gaia || true
-cd "$GOPATH/src/github.com/cosmos/gaia"
-git checkout -qf v0.5.0
+go get github.com/cosmos/cosmos-sdk || true
+cd "$GOPATH/src/github.com/cosmos/cosmos-sdk"
+git fetch --all
+git checkout -qf $RELEASETAG
+make get_tools
 make get_vendor_deps
 make install
 
+# Log installed versions
+echo "gaiad version :" $(gaiad version)
+echo "gaiacli version :" $(gaiacli version)
+
+
 # Get testnet configuration
-git clone https://github.com/tendermint/testnets /opt/testnets
+git clone https://github.com/cosmos/testnets /opt/testnets
 
 # Create gaia-daemon user and set permissions
 useradd --system --shell /usr/sbin/nologin gaia-daemon
 chown -R gaia-daemon:gaia-daemon /opt/testnets /var/log/gaia.log
+
+# TODO Move this to /opt/testnets/
+mkdir -p /opt/gaiad/config
+cp -a /opt/testnets/gaia-5000/. /opt/gaiad/config/gentx
+
+gaiad unsafe_reset_all --home=/opt/gaiad/
+gaiad init --gen-txs -o --chain-id=gaia-5000 --home=/opt/gaiad/
+
+chown -R gaia-daemon:gaia-daemon /opt/gaiad/
 
 # Create gaia.service
 echo "[Unit]
@@ -60,7 +87,9 @@ User=gaia-daemon
 # StandardError=file:/var/log/gaia.log
 
 # .. so use this workaround instead:
-ExecStart=/bin/sh -c '/opt/gopath/bin/gaia node start --home=/opt/testnets/gaia-2/gaia 2>&1 > /var/log/gaia.log'
+# ExecStart=/bin/sh -c '/opt/gopath/bin/gaia node start --home=/opt/testnets/gaia-2/gaia 2>&1 > /var/log/gaia.log'
+# ExecStart=/bin/sh -c '/opt/gopath/bin/gaiad node start --home=/opt/ testnets/gaia-2/gaia 2>&1 > /var/log/gaia.log'
+ExecStart=/bin/sh -c '/opt/gopath/bin/gaiad start --home=/opt/gaiad/ 2>&1 > /var/log/gaia.log'
 
 [Install]
 WantedBy=multi-user.target
